9c1623de9727b0042ffeb7b00b48053f0a9f1c87
jiangcai <jiangcai@milanoo.com>
2016-04-05 15:37:02 +0800

    mycart

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Version
+define('VERSION', '1.4.0.0');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+date_default_timezone_set('PRC');
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+$application_config = 'catalog';
+
+// Application
+require_once(DIR_SYSTEM . 'framework.php');

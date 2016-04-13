5b5db9bb331d0c887139b017103c0a291241e846
jiangcai <jiangcai@milanoo.com>
2016-04-13 13:58:00 +0800

    cart install

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://v2.yii.com/admin/');
+define('HTTP_CATALOG', 'http://v2.yii.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://v2.yii.com/admin/');
+define('HTTPS_CATALOG', 'http://v2.yii.com/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/me/yii/admin/');
+define('DIR_SYSTEM', 'E:/me/yii/system/');
+define('DIR_IMAGE', 'E:/me/yii/image/');
+define('DIR_LANGUAGE', 'E:/me/yii/admin/language/');
+define('DIR_TEMPLATE', 'E:/me/yii/admin/view/template/');
+define('DIR_CONFIG', 'E:/me/yii/system/config/');
+define('DIR_CACHE', 'E:/me/yii/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/me/yii/system/storage/download/');
+define('DIR_LOGS', 'E:/me/yii/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/me/yii/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/me/yii/system/storage/upload/');
+define('DIR_CATALOG', 'E:/me/yii/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myshopcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://v2.yii.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://v2.yii.com/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/me/yii/catalog/');
+define('DIR_SYSTEM', 'E:/me/yii/system/');
+define('DIR_IMAGE', 'E:/me/yii/image/');
+define('DIR_LANGUAGE', 'E:/me/yii/catalog/language/');
+define('DIR_TEMPLATE', 'E:/me/yii/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/me/yii/system/config/');
+define('DIR_CACHE', 'E:/me/yii/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/me/yii/system/storage/download/');
+define('DIR_LOGS', 'E:/me/yii/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/me/yii/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/me/yii/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myshopcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');

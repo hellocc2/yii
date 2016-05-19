852c84eb4a80a50b470c0f9859a2d24091b19701
jiangcai <jiangcai@milanoo.com>
2016-05-19 17:15:55 +0800

    new

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://v2.yii.com/admin/');
+define('HTTP_CATALOG', 'http://v2.yii.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://v2.yii.com/admin/');
+define('HTTPS_CATALOG', 'http://v2.yii.com/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\me\yii/admin/');
+define('DIR_SYSTEM', 'E:\me\yii/system/');
+define('DIR_DATABASE', 'E:\me\yii/system/database/');
+define('DIR_LANGUAGE', 'E:\me\yii/admin/language/');
+define('DIR_TEMPLATE', 'E:\me\yii/admin/view/template/');
+define('DIR_CONFIG', 'E:\me\yii/system/config/');
+define('DIR_IMAGE', 'E:\me\yii/image/');
+define('DIR_CACHE', 'E:\me\yii/system/cache/');
+define('DIR_DOWNLOAD', 'E:\me\yii/download/');
+define('DIR_LOGS', 'E:\me\yii/system/logs/');
+define('DIR_CATALOG', 'E:\me\yii/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myshoes');
+define('DB_PREFIX', 'oc_');
+?>
